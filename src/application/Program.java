73e34231570616bbbf3c31ea3f3207a6d3ddc116
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-01-14 14:38:50 -0200

    Declared lambda expression - version 2 (with variables)

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Predicate;
 
 import model.entities.Product;
@@ -10,6 +11,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("TV", 900.0));
@@ -17,13 +19,18 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
+		System.out.println("Type the value you want to exclude from the list: ");
+		double value = sc.nextDouble();
+		
+		Predicate<Product> pred = p -> p.getPrice() >= value;
 		
 		list.removeIf(pred);
 		
 		for(Product p : list) {
 			System.out.println(p);
 		}
+		
+		sc.close();
 
 	}
 
